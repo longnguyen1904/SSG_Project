3ebf4a012255b27f3617d4c29c51df625ae837d5
longnguyen1904 <190425lchd@gmail.com>
2025-02-24 15:38:14 +0700

    hg

--- Assets/Scripts/Player Control/Behavior.cs
+++ Assets/Scripts/Player Control/Behavior.cs
@@ -9,20 +9,10 @@ public class Behavior : MonoBehaviour {
     private float MINSPEED = 0f;
     private float decelerationRate = 5f;
     private float accelerationRate = 6f;
-
-    // gear true: số tiến, gear false: số lùi
     public bool gear = true;
-    
-    /*
-    force: Phát hiện va chạm
-    force true: khi va chạm, force false: khi không va chạm
-    */ 
-    public bool force = false;
 
-    // check false: Chỉ được lùi, check true: Chỉ được tiến
-    public bool check = true;
-
-    private void Awake() {
+    private void Awake()
+    {
         this.movement = GetComponent<Movement>();
 
         Debug.Log(movement == null ? "Movement is null" : "Movement is assigned");
@@ -34,7 +24,16 @@ public class Behavior : MonoBehaviour {
         Vector2 moveDirection = transform.right;
         // Set gear
         if (movement.speed == 0) {
-            setGear();     
+            if (Input.GetKey(KeyCode.J) && !gear) {
+                // Gear tiến
+                gear = true;
+                MAXSPEED = 30f;
+            }
+            if (Input.GetKey(KeyCode.K) && gear) {
+                // Gear lùi
+                gear = false;
+                MAXSPEED = 5f;
+            }            
         }
         // Update tốc theo deltaTime
         if (Input.GetKey(KeyCode.Space)) {
@@ -51,8 +50,12 @@ public class Behavior : MonoBehaviour {
 
         if (movement.speed > 0) {
             moveDirection = transform.right; // Hướng mũi xe
-
-            collisionHandler(moveDirection);
+            // Gear
+            if (gear) {
+                transform.position += (Vector3)(moveDirection * movement.speed * Time.deltaTime);
+            } else {
+                transform.position -= (Vector3)(moveDirection * movement.speed * Time.deltaTime);
+            }
 
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
                 currentRotation += rotationSpeed * Time.deltaTime;
@@ -67,60 +70,8 @@ public class Behavior : MonoBehaviour {
         // Cập nhật góc quay
         transform.rotation = Quaternion.Euler(0, 0, currentRotation);
 
-    }
+        
 
-    public void collisionHandler(Vector2 moveDirection) {
-        // Gear
-        if (!force) {
-            if (gear) {
-                moveForwad(moveDirection);
-            } else {
-                movebackWard(moveDirection);
-            }
-        } else {
-            setGear();
-            if (gear && check) {
-                moveForwad(moveDirection);
-            }
-            if (!(gear && check)) {
-                movebackWard(moveDirection);
-            }
-        }
-    }
-
-    public void moveForwad(Vector2 moveDirection) {
-        transform.position += (Vector3)(moveDirection * movement.speed * Time.deltaTime);
-    }
-
-    public void movebackWard(Vector2 moveDirection) {
-        transform.position -= (Vector3)(moveDirection * movement.speed * Time.deltaTime);
-    }
-
-    public void OnCollisionEnter2D() {
-        movement.speed = 0;
-        if (gear) {
-            this.check = false;
-            this.force = true;
-        } else {
-            this.check = true;
-            this.force = true;
-        }
-    }
-
-    public void setGear() {
-        // Gear tiến
-        if (Input.GetKey(KeyCode.J) && !gear) {
-            gear = true;
-            MAXSPEED = 30f;
-        }
-        // Gear lùi
-        if (Input.GetKey(KeyCode.K) && gear) {
-            gear = false;
-            MAXSPEED = 5f;
-        }       
-    }
-
-    public void OnCollisionExit2D() {
-        this.force = false;
+        
     }
 }
--- Assets/Scripts/Player Control/Movement.cs
+++ Assets/Scripts/Player Control/Movement.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
     public float speed = 0.0f;
     public LayerMask obstacleLayer;
     public Vector2 direction { get; private set;}
     public Vector2 nextDirection { get; private set;}
     
     
-    public void SetDirection(Vector2 direction)
+    public void SetDirection(Vector2 direction, bool forced = false)
     {
-        Debug.Log($"Setting direction to: {direction}");
+        Debug.Log($"Setting direction to: {direction}, Forced: {forced}");
         
-        if (Occupied(direction)) {
-            this.speed = 0.0f;
-        } 
+        if (forced || !Occupied(direction))
+        {
+            this.direction = direction;
+            this.nextDirection = Vector2.zero;
+        }
+        else
+        {
+            this.nextDirection = direction;
+        }
         
     }
 
@@ -23,6 +30,4 @@ public class Movement : MonoBehaviour {
         RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.obstacleLayer);
         return hit.collider != null;
     }
-
-   
 }
